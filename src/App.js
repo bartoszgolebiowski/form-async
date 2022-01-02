cdd97365902fc444f5d5e7309f9452e538c7b40d
golebiowskib <bartosz.golebiowski@@ttpsc.pl>
2022-01-02 23:24:39 +0100

    funny staff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,199 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actionRequest, selectRequest } from "./store";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const initialValues = {
+  user: null,
+  post: null,
+  comment: null,
+};
+
+const CLEAR = {
+  user: { post: null, comment: null },
+  post: { comment: null },
+  comment: {},
+};
+
+const URLS = {
+  user: (id) => `https://jsonplaceholder.typicode.com/users`,
+  post: (userId) =>
+    `https://jsonplaceholder.typicode.com/users/${userId}/posts`,
+  comment: (postId) =>
+    `https://jsonplaceholder.typicode.com/posts/${postId}/comments`,
+};
+
+const SELECT = {
+  user: (users) => users.map((user) => ({ id: user.id, name: user.name })),
+  post: (posts) => posts.map((post) => ({ id: post.id, name: post.title })),
+  comment: (comments) =>
+    comments.map((comment) => ({ id: comment.id, name: comment.name })),
+};
+
+const useSelectValues = (key, parentId = true) => {
+  const [isLoading, setLoading] = React.useState(false);
+  const [isError, setError] = React.useState(false);
+  const [values, setValues] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setError(false);
+        setLoading(true);
+        const response = await fetch(URLS[key](parentId));
+        const data = await response.json();
+        setValues(SELECT[key](data));
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    if (parentId) {
+      fetchData();
+    } else {
+      setValues(null);
+    }
+  }, [parentId, key]);
+
+  return { isLoading, isError, values };
+};
+
+const useSelectValuesRedux = (key, parentId = true) => {
+  const dispatch = useDispatch();
+  const data = useSelector(
+    selectRequest({
+      key,
+      arg: { parentId },
+      select: SELECT[key],
+    })
   );
-}
+
+  React.useEffect(() => {
+    if (!parentId) return;
+    dispatch(actionRequest({ key, arg: { parentId } }));
+  }, [parentId, key, dispatch]);
+
+  return {
+    isLoading: data?.isLoading ?? false,
+    isError: data?.isError ?? false,
+    values: data?.data ?? null,
+  };
+};
+
+const loading = {
+  opacity: 0.5,
+};
+
+const error = {
+  border: "1px solid red",
+  opacity: 0.5,
+};
+
+const Select = (props) => {
+  const { label, isLoading, isError, value, ...rest } = props;
+  const style = isLoading ? loading : isError ? error : null;
+
+  return (
+    <p>
+      <label>
+        {label}
+        <select {...rest} value={value ?? ""} style={style} />
+      </label>
+    </p>
+  );
+};
+
+const Options = (props) => {
+  const { values } = props;
+  return (
+    <>
+      <option style={{ display: "none" }} value=""></option>
+      {displayOptions(values)}
+    </>
+  );
+};
+
+const displayOptions = (values) => {
+  if (!values) return null;
+  return values.map((option) => (
+    <option key={option.id} value={option.id}>
+      {option.name}
+    </option>
+  ));
+};
+
+const App = () => {
+  const [values, setValues] = React.useState(initialValues);
+
+  const user = useSelectValuesRedux("user");
+  const post = useSelectValuesRedux("post", values.user);
+  const comment = useSelectValuesRedux("comment", values.post);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(values));
+  };
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+      ...CLEAR[e.target.name],
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} aria-label="Report">
+        <h1>Report</h1>
+        <Select
+          name="user"
+          label="User"
+          value={values.user}
+          onChange={handleChange}
+          disabled={!user.values}
+          isLoading={user.isLoading}
+          isError={user.isError}
+        >
+          <Options values={user.values} />
+        </Select>
+        <Select
+          name="post"
+          label="Post"
+          value={values.post}
+          onChange={handleChange}
+          disabled={!post.values}
+          isLoading={post.isLoading}
+          isError={post.isError}
+        >
+          <Options values={post.values} />
+        </Select>
+        <Select
+          name="comment"
+          label="Comment"
+          value={values.comment}
+          onChange={handleChange}
+          disabled={!comment.values}
+          isLoading={comment.isLoading}
+          isError={comment.isError}
+        >
+          <Options values={comment.values} />
+        </Select>
+        <button type="submit" disabled={!values.comment}>
+          Submit
+        </button>
+      </form>
+      <style>{`
+        form {
+          width: 20rem;
+          margin: 0 auto;
+        }
+        label {
+          display: flex;
+          flex-direction: column;
+        }
+      `}</style>
+    </>
+  );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,124 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { Provider } from "react-redux";
+import App from "./App";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { createStoreThunk } from "./store";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const customRender = (API) => {
+  return (ui, renderOptions = {}) => {
+    function Wrapper({ children }) {
+      const store = createStoreThunk(API);
+      return <Provider store={store}>{children}</Provider>;
+    }
+    return render(ui, { wrapper: Wrapper, ...renderOptions });
+  };
+};
+
+const MOCKED_API = {
+  user: ({ parentId }) =>
+    Promise.resolve([
+      {
+        id: 1,
+        name: "Leanne Graham",
+      },
+      {
+        id: 2,
+        name: "Ervin Howell",
+      },
+    ]),
+  post: ({ parentId }) =>
+    Promise.resolve([
+      {
+        userId: 2,
+        id: 11,
+        title: "et ea vero quia laudantium autem",
+      },
+      {
+        userId: 2,
+        id: 12,
+        title: "in quibusdam tempore odit est dolorem",
+      },
+    ]),
+  comment: ({ parentId }) =>
+    Promise.resolve([
+      {
+        postId: 12,
+        id: 56,
+        name: "et dolorem corrupti sed molestias",
+      },
+      {
+        postId: 12,
+        id: 57,
+        name: "qui quidem sed",
+      },
+    ]),
+};
+
+describe("form-async-redux", () => {
+  it("should render tree selects, user select should be enabled, post and comment should be disabled", async () => {
+    customRender(MOCKED_API)(<App />);
+    await waitFor(() => expect(screen.queryByLabelText(/User/i)).toBeEnabled());
+    expect(screen.getByLabelText(/Post/i)).toBeDisabled();
+    expect(screen.getByLabelText(/Comment/i)).toBeDisabled();
+  });
+
+  it("should enable post select, after selecting user from select input", async () => {
+    customRender(MOCKED_API)(<App />);
+    await waitFor(() => expect(screen.queryByLabelText(/User/i)).toBeEnabled());
+    userEvent.selectOptions(screen.getByLabelText(/User/i), "Leanne Graham");
+    await waitFor(() => expect(screen.queryByLabelText(/Post/i)).toBeEnabled());
+  });
+
+  it("should enable comment select, after selecting post from select input", async () => {
+    customRender(MOCKED_API)(<App />);
+    await waitFor(() => expect(screen.queryByLabelText(/User/i)).toBeEnabled());
+    userEvent.selectOptions(screen.getByLabelText(/User/i), "Leanne Graham");
+    await waitFor(() => expect(screen.queryByLabelText(/Post/i)).toBeEnabled());
+    userEvent.selectOptions(
+      screen.getByLabelText(/Post/i),
+      "et ea vero quia laudantium autem"
+    );
+    await waitFor(() =>
+      expect(screen.queryByLabelText(/Comment/i)).toBeEnabled()
+    );
+  });
+
+  it("should clear post and comment selects, post select should be enabled, comment select should be disabled", async () => {
+    customRender(MOCKED_API)(<App />);
+    await waitFor(() => expect(screen.queryByLabelText(/User/i)).toBeEnabled());
+    userEvent.selectOptions(screen.getByLabelText(/User/i), "Leanne Graham");
+    await waitFor(() => expect(screen.queryByLabelText(/Post/i)).toBeEnabled());
+    userEvent.selectOptions(
+      screen.getByLabelText(/Post/i),
+      "et ea vero quia laudantium autem"
+    );
+    await waitFor(() =>
+      expect(screen.queryByLabelText(/Comment/i)).toBeEnabled()
+    );
+    userEvent.selectOptions(screen.getByLabelText(/User/i), "Ervin Howell");
+    expect(screen.getByLabelText(/Post/i)).toHaveValue("");
+    expect(screen.getByLabelText(/Comment/i)).toBeDisabled();
+    expect(screen.getByLabelText(/Comment/i)).toHaveValue("");
+  });
+
+  it("should enable submit button, when all selects are filled", async () => {
+    customRender(MOCKED_API)(<App />);
+    await waitFor(() => expect(screen.queryByLabelText(/User/i)).toBeEnabled());
+    userEvent.selectOptions(screen.getByLabelText(/User/i), "Leanne Graham");
+    await waitFor(() => expect(screen.queryByLabelText(/Post/i)).toBeEnabled());
+    userEvent.selectOptions(
+      screen.getByLabelText(/Post/i),
+      "et ea vero quia laudantium autem"
+    );
+    await waitFor(() =>
+      expect(screen.queryByLabelText(/Comment/i)).toBeEnabled()
+    );
+    userEvent.selectOptions(
+      screen.getByLabelText(/Comment/i),
+      "qui quidem sed"
+    );
+    await waitFor(() =>
+      expect(screen.queryByRole("button", { name: /submit/i })).toBeEnabled()
+    );
+  });
 });
